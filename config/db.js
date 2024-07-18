6112f3b6d8f8294c577a49da474fc63cb5ae93fd
suraj mane <mane315161@gmail.com>
2024-07-18 17:00:48 +0530

    update project

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 // Connect to MongoDB
-const connectDb = async (req,res)=>{
-    try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/person")
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(error);
-    }
-}
+const connectDb = async (req, res) => {
+  try {
+    // const conn = await mongoose.connect("mongodb://localhost:27017/person")
+    const conn = await mongoose.connect(process.env.MANGODB_URL);
+    // "mongodb+srv://mane315161:Suraj1234@cluster0.svhrzst.mongodb.net/"
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 module.exports = connectDb;
